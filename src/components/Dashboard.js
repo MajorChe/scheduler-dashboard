2c3304a72287310b9868cd7b1469e1769ba0fc96
MajorChe <charithnanduri03@gmail.com>
2021-12-16 19:18:35 -0500

    add Panel component and onclick functionality

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,44 +1,80 @@
 import React, { Component } from "react";
 import classnames from "classnames";
 import Loading from "./Loading";
+import Panel from "./Panel";
 
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    value: 6,
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    value: "1pm",
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    value: "Wednesday",
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
-  }
+    value: "2.3",
+  },
 ];
 
 class Dashboard extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.selectPanel = this.selectPanel.bind(this);
+  // }
 
   state = {
-    loading : false
+    loading: false,
+    focused: null,
+  };
+
+  selectPanel(id) {
+      this.setState({
+        focused: id,
+      });
   }
+
   render() {
-    const dashboardClasses = classnames("dashboard");
-    if(this.state.loading) {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused,
+    });
+
+    if (this.state.loading) {
       return <Loading />;
     }
-    return (
-        <main className={dashboardClasses}>
-        </main>
-    );
+
+    const panelArr = data
+      .filter(
+        (panel) =>
+          this.state.focused === null || this.state.focused === panel.id
+      )
+      .map((panel) => {
+        return (
+          <Panel
+            key={panel.id}
+            label={panel.label}
+            value={panel.value}
+            onSelect={(event) => {
+              if (this.state.focused === panel.id) {
+                return this.selectPanel(null)
+              } else {
+                return this.selectPanel(panel.id)
+              }
+            }}
+          />
+        );
+      });
+
+    return <main className={dashboardClasses}>{panelArr}</main>;
   }
 }
 
